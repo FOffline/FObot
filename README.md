53bca94bfec2e263edfdbd150f0d87d93726fc74
Koniko <5898219+Koniiko@users.noreply.github.com>
2020-05-21 14:47:29 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # fonline-bot
 Simple FOnline Discord status bot powered by nodejs.
+#
+To setup the enviroment simply run:
+``` npm install```
+while inside the root of repository.
+
+To run an instance of bot fill out nessesary fields in ```config.json``` and run:
+```node bot.js```
+#
+Dependencies:
+
+
+1. npm
+1. nodejs
+   1. discord.js
+   1. net
---
+++ bot.js
@@ -0,0 +1,55 @@
+const config   = require("./config.json");
+const net      = require('net');
+const Discord  = require('discord.js'); 
+const client   = new Discord.Client();  
+
+client.on("ready", function()
+{
+    bot_FOnline();
+});
+
+client.login(config.token);
+
+let buff = new Buffer([0xFF, 0xFF, 0xFF, 0xFF]); 
+
+function bot_FOnline()
+{
+    var connection = new net.Socket();
+    connection.setTimeout(10000);
+	
+    connection.connect(config.serverPort, config.serverAddress, function () 
+	{
+        console.log(`Writing ${buff}`)
+        connection.write(buff); 
+    });
+    
+	connection.on('data', function (data) 
+	{
+        console.log('Received: ' + data);
+
+        var buffer = new Buffer('', 'hex');
+        buffer = Buffer.concat(
+        [
+            buffer, new Buffer(data, 'hex')
+        ]);
+
+        online = buffer.readUInt32LE(0);
+        
+        console.log("Online is " +online)
+
+        if (online != '') 
+		{
+            client.user.setActivity("Online: " +online)
+        }
+        connection.destroy();
+    });
+            
+	connection.on('error', function (err) 
+	{
+        client.user.setActivity("Bad omen! Can't reach Core!")
+        connection.destroy();
+    });
+
+	setTimeout(bot_FOnline, 50000);
+}
+
